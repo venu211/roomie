51f4d5d2bacadcfee881da690ccdb4e778cb4071
Venumadhav Dondapati <venumadhavdondapati@Venumadhavs-MacBook-Pro.local>
2017-12-09 17:36:02 -0500

    Database & omniauth for twitter

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  match 'auth/:provider/callback', to: 'sessions#create', :via => [:get]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
