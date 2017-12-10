efc9214d8c264d5589e8eedb6cf1cf7b9e725fb0
Venumadhav Dondapati <venumadhavdondapati@Venumadhavs-MacBook-Pro.local>
2017-12-10 01:24:11 -0500

    Add Gmail and facebook Oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
+
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  #puts "key: #{ENV['TWITTER_KEY']}\n secret: #{ENV['TWITTER_SECRET']}"
-end
+  provider :twitter,       ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']#, {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :facebook,      ENV['FACEBOOK_APP_ID'],  ENV['FACEBOOK_APP_SECRET']
+  end
