712e25f28cf40d20e423929036f5d94b90eb577a
Venumadhav Dondapati <venumadhavdondapati@Venumadhavs-MacBook-Pro.local>
2017-12-24 23:09:30 -0500

    add account controller action

--- config/unicorn/development.rb
+++ config/unicorn/development.rb
@@ -1,11 +1,11 @@
-root = "/home/deployer/roomie/current"
+root = "/Users/venumadhavdondapati/workspace/roomie"#{}"/home/deployer/roomie/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
  
 listen "/tmp/unicorn.sock"
-worker_processes 3
+worker_processes 2
 timeout 30
  
 preload_app true
